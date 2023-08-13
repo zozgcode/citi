e8fadd50d2818c3182b8987d635ac7939d2211a3
sunshine-web-coder <webdev4gaming@gmail.com>
2023-08-13 08:32:47 +0100

    first commit

---
+++ src/components/userDB.jsx
@@ -0,0 +1,32 @@
+const users = [
+  { 
+    id: 1,
+    name: "Karen Hayes",
+    cardName: "Karen Hayes ",
+    username: "Loveyoujimmy1@",
+    password: "SuccessfullHayes111@",
+    amount: "2,100,000.00",
+    savingAccountName: "Jimmie E Clay",
+    savingAccountAmount: "1,020,000.00",
+  },
+  { 
+    id: 2,
+    name: "Stephanie Sarah Jensen",
+    cardName: "Stephanie Sarah J.",
+    username: "Driver29",
+    password: "Billycompany3048",
+    amount: "2,586,094.00",
+  },
+  { 
+    id: 3,
+    name: "John Irwin",
+    cardName: "John Irwin",
+    username: "JohnSavings",
+    password: "Jackson1993",
+    amount: "25,000,000.00",
+  },
+];
+export default users;
+
+
+
